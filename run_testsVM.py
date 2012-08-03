2afd28d9083eced6387baddd9740028c90131a6c
Guillermo Carrasco <guille.ch.88@gmail.com>
2012-08-03 16:20:43 +0200

    Pulls and TRY to install the pipeline

--- run_testsVM.py
+++ run_testsVM.py
@@ -4,7 +4,21 @@ It creates a new VM using vagrant and then uses it to pull and install the cloud
 the full pipeline. Then performs the tests.
 """
 
-from fabric.api import local
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+#Define the vagrant host
+env.hosts = ['vagrant@localhost:2222']
+
+
+def install_pipeline():
+    """Pulls and install the pipeline within the vagrant visrtual machine"""
+
+    run("git clone https://github.com/chapmanb/bcbb.git")
+    codedir = '/home/vagrant/bcbb/nextgen'
+    with cd(codedir):
+        run("sudo python setup.py install")
 
 
 def install_cloudbiolinux():
@@ -17,14 +31,22 @@ def install_cloudbiolinux():
 def install_VM():
     """Download, init, provide and up a vagrant VM able to run the pipeline"""
 
-    local("vagrant box add debian_squeeze_32 http://mathie-vagrant-boxes.s3.amazonaws.com/debian_squeeze_32.box")
+    #Error handling, just in case that already exists a vagrant box with the same name
+    with settings(warn_only=True):
+        result = local("vagrant box add debian_squeeze_32 http://mathie-vagrant-boxes.s3.amazonaws.com/debian_squeeze_32.box", capture=True)
+    if result.failed and not confirm("It looks like already exists a vagrant box with this name in your system, do you want to continue using this box? (y/n)"):
+        abort("Aborting at user request")
     local("vagrant up")
 
 
-## Main script
-print("Prepare and init the Virtual Machine...")
-install_VM()
-print("DONE")
-print("Preparing dependencies...")
-install_cloudbiolinux()
-print("DONE")
+## Main method
+def deploy():
+    print("Prepare and init the Virtual Machine...")
+    install_VM()
+    print("DONE")
+    print("Preparing dependencies...")
+    install_cloudbiolinux()
+    print("DONE")
+    print("Installing the pipeline...")
+    install_pipeline()
+    print("DONE")
