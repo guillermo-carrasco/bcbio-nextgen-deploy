11afaafe639ec6f02c24576667ba826e9c117b34
Guillermo Carrasco <guille.ch.88@gmail.com>
2012-10-09 13:48:26 +0200

    Install pipeline dependencies directly form here, not using provisioner. Using deploy_non_root.py to install the pipeline. [ci skip]

--- deploy_on_vm.py
+++ deploy_on_vm.py
@@ -9,20 +9,21 @@ Then performs the tests.
 from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
+from fabric.operations import sudo
 import os
 
 #Define the vagrant host
 env.hosts = ['vagrant@localhost:1234']
 env.user = 'vagrant'
 env.password = 'vagrant'
-codedir = '/home/vagrant/bcbb_chapmanb/nextgen'
+codedir = '/home/vagrant/bcbio-nextgen-deploy'
 
 def install_pipeline():
-    """Pulls and install the pipeline within the vagrant visrtual machine"""
+    """Pulls and install the pipeline within the vagrant virtual machine"""
 
-    run("git clone https://github.com/guillermo-carrasco/bcbb_chapmanb.git")
+    run("git clone https://github.com/guillermo-carrasco/bcbio-nextgen-deploy.git")
     with cd(codedir):
-        run("sudo python setup.py install")
+        run("python deploy_non_root.py install")
 
 
 def install_VM():
@@ -30,10 +31,31 @@ def install_VM():
 
     #Error handling, just in case that already exists a vagrant box with the same name
     with settings(warn_only=True):
-        result = local("vagrant box add precise32 http://files.vagrantup.com/precise64.box", capture=True)
+        result = local("vagrant box add precise64 http://files.vagrantup.com/precise64.box", capture=True)
     if result.failed and not confirm("It looks like already exists a vagrant box with this name in your system, do you want to continue using this box? (y/n)"):
         abort("Aborting at user request")
     local("vagrant up")
+
+
+def provision_VM():
+    """Install necessary software to run the pipeline in the virtual machine"""
+    #Add repositories
+    sudo('apt-get update')
+    sudo('apt-get install -y python-software-properties')
+    sudo('add-apt-repository -y ppa:scilifelab/scilifelab')
+    sudo('add-apt-repository -y ppa:debian-med/ppa')
+    sudo('apt-get update')
+    #Install pipeline dependencies
+    sudo('apt-get install -y snpeff picard-tools bwa bowtie bowtie2 \
+              freebayes fastqc-0.10.1 gatk r-base texlive texlive-latex-extra \
+              tophat openjdk-6-jre samtools unzip lftp cufflinks wigtools \
+              python-pip python-dev python-setuptools python-nose git')
+    run('wget http://sourceforge.net/projects/snpeff/files/snpEff_v2_0_5_core.zip && unzip snpEff_v2_0_5_core.zip')
+    sudo('mv snpEff_2_0_5 /usr/share/java/snpeff')
+    sudo('sed \'s/\.\/data\//\/usr\/share\/snpEff\/data/\' < /usr/share/java/snpeff/snpEff.config > /usr/share/java/snpeff/snpEff.config_ && \
+              mv /usr/share/java/snpeff/snpEff.config_ /usr/share/java/snpeff/snpEff.config')
+    run('lftp -e \'pget -n 8 http://downloads.sourceforge.net/project/snpeff/databases/v2_0_5/snpEff_v2_0_5_GRCh37.63.zip; quit\'')
+    sudo('unzip snpEff_v2_0_5_GRCh37.63.zip -d /usr/share/snpEff/ && rm snpEff_v2_0_5_GRCh37.63.zip')
 
 
 ## Main method
@@ -41,10 +63,8 @@ def deploy():
     print("Prepare and init the Virtual Machine...")
     install_VM()
     print("DONE")
-    print("Installing the pipeline...")
+    print("Provisioning Virtual Machine with software dependencies...")
+    provision_VM()
+    print("Installing the pipeline and running the testsuite...")
     install_pipeline()
     print("DONE")
-    print("Running tests")
-    with cd(os.path.join(codedir, 'tests')):
-    	run("nosetests -s -v")
-    run("DONE")
