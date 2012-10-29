776bba62a31f2fc455b95e324f8d76abf1de09c0
Guillermo Carrasco <guille.ch.88@gmail.com>
2012-10-29 11:15:53 +0100

    Fixed __new__ overriding

--- moduleSystem.py
+++ moduleSystem.py
@@ -1,18 +1,20 @@
 import os
 import subprocess
 
-class moduleManager:
+class moduleManager(object):
     """Class to list and manage the different modules in a session
        (modules.sourceforge.net)
     """
-    def __new__(self):
+    def __new__(typ, *args, **kwargs):
         """Check if the module system is installed before cerating an instance
            of the class
         """
         try:
             os.environ['modules_shell']
+            return moduleManager
         except KeyError:
             raise RuntimeError('No module system found!')
+            return None
 
     def __init__(self):
         self.env = dict(os.environ)
